f0f159418e1db695051e9b4cff0d714cc4572a88
Василий Худобин <khudobin_v@vk.com>
2024-04-11 15:24:52 +0300

    Фиксы

--- auth.ts
+++ auth.ts
@@ -10,29 +10,6 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
-  callbacks: {
-    async session({ token, session }) {
-      console.log( session.user.role)
-      if (token.sub && session.user) {
-        session.user.id = token.sub;
-      }
-      
-      return session  
-    },
-
-    async jwt({ token }) {
-      if (!token.sub) return token
-      
-      const existingUser = await getUserById(token.sub);
-
-      if (!existingUser) return token;
-
-      token.role = existingUser.role;
-
-      return token
-    }
-  
-  },
   adapter: PrismaAdapter(db),
   session: {
     strategy: "jwt",
