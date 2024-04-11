2f9c1b86ae598fd6edb71cf9e4e219e6c66c2a6f
Василий Худобин <khudobin_v@vk.com>
2024-04-11 18:18:45 +0300

    Фикс вариантов номера маршрута

--- components/transport/route-number.tsx
+++ components/transport/route-number.tsx
@@ -2,15 +2,30 @@
 
 interface RouteNumberProps {
   number: string;
-  color: string;
+  variant: "red" | "blue" | "green";
 }
 
-export const RouteNumber = ({ number, color }: RouteNumberProps) => {
+export const RouteNumber = ({ number, variant }: RouteNumberProps) => {
   return (
-    <div
-      className={`bg-${color}-600 rounded-[30%] px-3 py-3 w-6 h-6 flex items-center justify-center`}
-    >
-      <p className="text-white font-semibold text-sm">{number}</p>
-    </div>
+    <>
+      {variant === "red" && (
+        <div className="bg-red-600 rounded-[30%] px-3 py-3 w-6 h-6 flex items-center justify-center">
+          <p className="text-white font-semibold text-sm">{number}</p>
+        </div>
+      )}
+      {variant === "blue" && (
+        <div className="bg-blue-600 rounded-[30%] px-3 py-3 w-6 h-6 flex items-center justify-center">
+          <p className="text-white font-semibold text-sm">{number}</p>
+        </div>
+      )}
+      {variant === "green" && (
+        <div className="bg-green-600 rounded-[30%] px-3 py-3 w-6 h-6 flex items-center justify-center">
+          <p className="text-white font-semibold text-sm">{number}</p>
+        </div>
+      )}
+      <div className="rounded-[30%] px-3 py-3 w-6 h-6 flex items-center justify-center">
+        <p className="text-white font-semibold text-sm">{number}</p>
+      </div>
+    </>
   );
 };
