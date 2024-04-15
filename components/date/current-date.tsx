42da447bbe2f2165dd7509b91a4727479f498e36
Василий Худобин <khudobin_v@vk.com>
2024-04-16 00:23:46 +0300

    Правка времени

--- components/date/current-date.tsx
+++ components/date/current-date.tsx
@@ -13,7 +13,9 @@ export const CurrentDate = () => {
   return (
     <div className="bg-opacity border px-4 py-2 rounded-lg flex flex-col gap-1 w-full sm:w-1/3">
       {/* <h4 className="text-xl">Время сейчас:</h4> */}
-      <h3 className="text-2xl font-semibold">{date.toLocaleTimeString()}</h3>
+      <h3 className="text-2xl font-semibold">
+        {date.toLocaleTimeString([], { hour: "numeric", minute: "numeric" })}
+      </h3>
       <h3 className="text-xl font-semibold">
         {date.toLocaleDateString("ru-RU", {
           weekday: "long",
